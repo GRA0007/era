3249a242f212e32093c3519668d7c2da73268851
Benjamin Grant <benjamin.grantGRA0007@gmail.com>
2019-04-30 14:48:53 +1000

    Google signin

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let menu_darkMode = document.getElementById('dark_mode');
+let menu_logOut = document.getElementById('log_out');
 
 menu_darkMode.onclick = function() {
 	if (document.body.classList.contains('dark')) {
@@ -10,3 +11,31 @@ menu_darkMode.onclick = function() {
 	}
 	return false;
 };
+
+menu_logOut.onclick = function() {
+	let auth2 = gapi.auth2.getAuthInstance();
+	auth2.signOut().then(function() {
+		// Redirect back
+		window.location.replace('index.html');
+	});
+	return false;
+};
+
+function gapiLoad() {
+	gapi.load('auth2', function() {
+		gapi.auth2.init().then(function() {
+			let auth2 = gapi.auth2.getAuthInstance();
+			if (auth2.isSignedIn.get()) {
+				let profile = auth2.currentUser.get().getBasicProfile();
+				document.getElementById('profile_image').src = profile.getImageUrl();
+				document.getElementById('profile_image').title = "Logged in as " + profile.getName();
+			} else {
+				window.location.replace('index.html');
+			}
+		});
+	});
+}
+
+function onSignIn() {
+	window.location.replace('app.html');
+}
